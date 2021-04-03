04ae4ccecc29148350868bcadea7d6a27b8e4555
alm92387 <81530707+alm92387@users.noreply.github.com>
2021-04-03 19:50:22 -0400

    added icon

--- src/index.js
+++ src/index.js
@@ -14,13 +14,18 @@ function showTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name; 
   document.querySelector("#temperature").innerHTML = `${temperature}`; 
   
-  
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+      let iconElement = document.querySelector("#ticon");
+   iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
